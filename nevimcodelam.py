44380978c7ac6d924e1185ab915587ba7e2af735
Alper <75456808+ItsAlper@users.noreply.github.com>
2022-05-06 13:24:07 +0200

    New file

---
+++ nevimcodelam.py
@@ -0,0 +1,32 @@
+class hero:
+    def _init_(self,name ="Ranger",born =1960,weapons = "Luk",bestactorname = "Josef Rosa")
+        self.__name = name
+        self.__born = born
+        self.__weapons = weapons
+        self.__bestactor = bestactor
+    def setName(self,name):
+        self.__name = name
+    def setBorn(self,born):
+        self.__born = born
+    def addweapons(self,weapons):
+        self.__addweapons = weapons
+    def bestactor(self,bestactor):
+        self.__bestactor = bestactor
+    def getname(self):
+        print(self.name)
+    def getborn(self):
+        print(self.born)
+    def getbestactorname(self):
+        print(self.bestactorname)
+    def addweapon(self,weapons):
+        self.__weapon = weapon
+    def removeweapons(self,weapons):
+        self.__weapons.remove = (weapons)
+    def getweapons(self):
+        print(self.weapons)
+cikan = hero:
+cikan.getname()
+cikan.getborn()
+cikan.getweapons()
+cikan.addweapon("tatarka")
+cikan.getweapons()
